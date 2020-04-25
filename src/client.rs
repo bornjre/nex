f62591c171f8d0cf83f4fab8de32e3d407f89e97
Manoj <bornjre@gmail.com>
2020-04-25 23:25:09 +0545

    basic grpc hello world

---
+++ src/client.rs
@@ -0,0 +1,21 @@
+use nexproto::greeter_client::GreeterClient;
+use nexproto::HelloRequest;
+
+pub mod nexproto {
+    tonic::include_proto!("nexproto");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
