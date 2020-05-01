014f2c86a5913c61e99c27fd2c6fd929f4ad303f
Manoj <bornjre@gmail.com>
2020-05-01 20:29:48 +0545

    basic layot complete

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,12 @@
 use nexproto::greeter_client::GreeterClient;
-use nexproto::HelloRequest;
+use nexproto::work_provider_client::{WorkProviderClient};
+use nexproto::{HelloRequest, WorkRequest, ResultType};
 
 pub mod nexproto {
     tonic::include_proto!("nexproto");
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn greeter_hello() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
     let request = tonic::Request::new(HelloRequest {
@@ -14,8 +14,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let response = client.say_hello(request).await?;
-
     println!("RESPONSE={:?}", response);
+    Ok(())
+}
+
+
+async fn do_work() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = WorkProviderClient::connect("http://[::1]:50051").await?;
+    
+    let mut counter:i32 = 0;
+
+    loop {
+        counter = counter + 1;
+        if counter > 3 {
+            break;
+        }
+
+        let request = tonic::Request::new(WorkRequest{
+            r#type: "any".to_string(),
+        });
+    
+        let mut response = client.need_work(request).await?;
+    
+        let response = response.get_mut();
+        
+        println!("do work {}", response.input);
+
+        let output = response.input * response.input;
+
+        let request = tonic::Request::new(ResultType{
+            status: 0,
+            output:output,
+        });
+    
+        let mut response = client.upload_result(request).await?;
+        println!("RESPONSE={:?}", response);
+
+    }
+
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    do_work().await;
+    //println!(" {:?}", greeter_hello().await);
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,17 @@
 use tonic::{transport::Server, Request, Response, Status};
 
 use nexproto::greeter_server::{Greeter, GreeterServer};
-use nexproto::{HelloReply, HelloRequest};
+use nexproto::{
+    HelloReply,
+    HelloRequest,
+    WorkRequest,
+    Work,
+    ResultType,
+    ResultAck
+ };
+
+use nexproto::work_provider_server::{WorkProvider, WorkProviderServer };
+
 
 pub mod nexproto {
     tonic::include_proto!("nexproto");
@@ -26,13 +36,36 @@ impl Greeter for MyGreeter {
     }
 }
 
+#[derive(Debug, Default)]
+struct WorkService {}
+
+#[tonic::async_trait]
+impl WorkProvider for WorkService {
+    async fn need_work( &self, request: Request<WorkRequest>) -> Result<Response<Work>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        Ok(Response::new(Work{input: 9}))
+    }
+
+    async fn upload_result( &self,request: Request<ResultType>) -> Result<Response<ResultAck>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        Ok(Response::new(ResultAck{status:0, msg:"seems ok".to_string() }))
+    }
+
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let greeter = MyGreeter::default();
+    let workp = WorkService::default();
 
     Server::builder()
         .add_service(GreeterServer::new(greeter))
+        .add_service(WorkProviderServer::new(workp))
         .serve(addr)
         .await?;
 
